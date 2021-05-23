ff4a5430cd136654607980ac6e7d3902ed4657cb
pemudakoding <pemudakoding@gmail.com>
2021-05-23 20:44:48 +0800

    added: validate at contact routes

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const {  validationResult,check } = require('express-validator');
 const { 
     storeContact,
     loadContacts ,
@@ -18,41 +19,90 @@ router
             });
         })
     })
-    .post('/',(req,res) => {
-        storeContact(req.body,(head,data) => {
-            res.send({
-                head,
-                data,
-            });
-        })
+    .post('/',[
+        check('name')
+            .notEmpty().withMessage('Nama tidak boleh kosong').bail()
+            .isLength({min:3}).withMessage('Nama minimal 3 karakter').bail()
+            .isString(),
+        check('email')
+            .notEmpty().withMessage('Email tidak boleh kosong').bail()
+            .isEmail().withMessage('Email tidak valid').bail(),
+        check('number')
+            .notEmpty().withMessage('Nomor tidak boleh kosong').bail().
+            isMobilePhone('id-ID').withMessage('nomor tidak valid')
+
+    ],(req,res) => {
+
+        const errors = validationResult(req);
+        if(errors.isEmpty()){
+            storeContact(req.body,(head,data) => {
+                res.send({
+                    head,
+                    data,
+                });
+            })
+        }else{
+            return res.status(400).json({errors:errors.array()});
+        }
     })
-    .get('/:id', (req,res) => {
 
-        detailContact(req.params.id,(head,data) => {
-            res.send({
-                head,
-                data,
+    .get('/:id',[
+      check('id')
+        .notEmpty().withMessage('id tidak boleh kosong').bail()
+        .isAlphanumeric().withMessage('Id tidak valid'),
+    ], (req,res) => {
+
+        const errors = validationResult(req);
+        if(errors.isEmpty()){
+            detailContact(req.params.id,(head,data) => {
+                res.send({
+                    head,
+                    data,
+                });
             });
-        });
+        }else{
+            return res.status(400).json({errors:errors.array()}); 
+        }
     })
-    .put('/:id',(req,res) => {
+    .put('/:id',[
+        check('id')
+          .notEmpty().withMessage('id tidak boleh kosong').bail()
+          .isAlphanumeric().withMessage('Id tidak valid'),
+      ],(req,res) => {
 
-        updateContact(req.params.id,req.body,(head,data) => {
+        const errors = validationResult(req);
 
-            res.send({
-                head,
-                data,
+        if(errors.isEmpty()){
+            updateContact(req.params.id,req.body,(head,data) => {
+                res.send({
+                    head,
+                    data,
+                });
             });
-        });
+        }else{
+            return res.status(400).json({errors:errors.array()}); 
+        }
+        
     })
-    .delete('/:id', (req,res) => {
+    .delete('/:id',[
+        check('id')
+          .notEmpty().withMessage('id tidak boleh kosong').bail()
+          .isAlphanumeric().withMessage('Id tidak valid'),
+      ], (req,res) => {
 
-        destroyContact(req.params.id,(head,data) => {
-            res.send({
-                head,
-                data,
+        const errors = validationResult(req);
+
+        if(errors.isEmpty()){
+            destroyContact(req.params.id,(head,data) => {
+                res.send({
+                    head,
+                    data,
+                });
             });
-        });
+        }else{
+            return res.status(400).json({errors:errors.array()}); 
+        }
+        
     });
     
 module.exports = router;
